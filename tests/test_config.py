561cd259bc166a6c2a744ffa9c236f175337ea3f
German Gomez-Herrero <g@germangh.com>
2015-11-20 18:52:58 +0100

    Make test_config idempotent

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,6 +2,7 @@
 
 
 import pytest
+import uuid
 from s3keyring.s3 import S3Keyring
 
 
@@ -10,14 +11,25 @@ def config(scope='module'):
     return S3Keyring().config
 
 
+@pytest.yield_fixture
+def dummyparam(config, scope='module'):
+    yield 'dummyparam'
+    config.config.remove_option('default', 'dummyparam')
+
+
+@pytest.fixture
+def dummyvalue():
+    return str(uuid.uuid4())
+
+
 def test_read_config(config):
     """Sets value for an existing configuration option"""
     profile_name = config.get('default', 'profile')
     assert profile_name == 'default'
 
 
-def test_write_config(config):
-    config.set('default', 'dummyparam', 'anothervalue')
+def test_write_config(config, dummyparam, dummyvalue):
+    config.set('default', dummyparam, dummyvalue)
     config.save()
     config.load()
-    assert config.get('default', 'dummyparam') == 'anothervalue'
+    assert config.get('default', dummyparam) == dummyvalue
