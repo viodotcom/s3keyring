50519406ac64766874ce9edf5cea69233461ffb2
German Gomez-Herrero <g@germangh.com>
2016-01-02 20:42:08 +0100

    Test custom configuration file feature

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,6 +3,8 @@
 
 import pytest
 import uuid
+import tempfile
+import os
 from s3keyring.s3 import S3Keyring
 
 
@@ -17,9 +19,17 @@ def dummyparam(config, scope='module'):
     config.config.remove_option('default', 'dummyparam')
 
 
+@pytest.yield_fixture
+def dummy_config_file():
+    filename = os.path.join(tempfile.gettempdir(), str(uuid.uuid4()))
+    yield filename
+    if os.path.isfile(filename):
+        os.remove(filename)
+
+
 @pytest.fixture
-def dummyvalue():
-    return str(uuid.uuid4())
+def custom_config_file(dummy_config_file, scope='module'):
+    return S3Keyring(profile_name='test', config_file=dummy_config_file).config
 
 
 def test_read_config(config):
@@ -28,8 +38,29 @@ def test_read_config(config):
     assert profile_name == 'default'
 
 
-def test_write_config(config, dummyparam, dummyvalue):
+def test_write_config(config, dummyparam):
+    dummyvalue = str(uuid.uuid4())
     config.set('default', dummyparam, dummyvalue)
     config.save()
     config.load()
     assert config.get('default', dummyparam) == dummyvalue
+
+
+def test_read_custom_config_file(custom_config_file, dummy_config_file):
+    """Reads a parameter from a custom config file"""
+    profile_name = custom_config_file.get('default', 'profile')
+    assert profile_name == 'default'
+    assert custom_config_file.config_file == dummy_config_file
+    assert os.path.isfile(dummy_config_file)
+
+
+def test_write_config_in_custom_config_file(custom_config_file, dummyparam,
+                                            config):
+    dummyvalue = str(uuid.uuid4())
+    custom_config_file.set('default', dummyparam, dummyvalue)
+    custom_config_file.save()
+    custom_config_file.load()
+    assert custom_config_file.get('default', dummyparam) == dummyvalue
+    assert config.config_file != custom_config_file.config_file
+    config.load()
+    assert config.get('default', dummyparam) != dummyvalue
