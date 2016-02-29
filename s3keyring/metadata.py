1bca602b18b8ac4f70c2ffd232f637c57a02fce3
Ilya Kalinin <ilya.kalinin@adroll.com>
2016-02-29 14:04:08 -0800

    bump version number

--- s3keyring/metadata.py
+++ s3keyring/metadata.py
@@ -8,7 +8,7 @@ Information describing the project.
 package = 's3keyring'
 project = "S3 backend for the keyring module"
 project_no_spaces = project.replace(' ', '')
-version = '0.6.0.post1'
+version = '0.6.0.post2'
 description = 'Keeps your secrets safe in S3'
 authors = ['German Gomez-Herrero']
 authors_string = ', '.join(authors)
