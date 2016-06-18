e3a396305ef3b9355b1e656dc747de1f9d3eccd9
German Gomez-Herrero <german@innovativetravel.eu>
2016-06-18 14:33:50 +0200

    Add keyring method to list the secrets associated to a service.

--- s3keyring/cli.py
+++ s3keyring/cli.py
@@ -5,31 +5,23 @@ from __future__ import print_function
 import click
 import s3keyring.s3
 from s3keyring.settings import config
-import os
 
 
-def _get_default_config_file():
-    """Gets the full path to the default config file"""
-    project_file = os.path.join(os.path.curdir, '.s3keyring.ini')
-    if os.path.isfile(project_file):
-        return project_file
-    else:
-        return os.path.join(os.path.expanduser('~'), '.s3keyring.ini')
-
-
-@click.group(name='s3keyring')
-@click.option("--profile", default='default', metavar='NAME',
+@click.group(name="s3keyring")
+@click.option("--profile", default='default', metavar="NAME",
               help="The name of configuration profile.")
+@click.option("--ini", default=None, metavar="INIFILE",
+              help="Path to the keyring configuration file.")
 @click.pass_context
-def main(ctx, profile):
+def main(ctx, profile, ini):
     """S3 backend for Python's keyring module."""
     config.boto_config.activate_profile(profile)
-    kr = s3keyring.s3.S3Keyring()
-    ctx.obj = {'keyring': kr}
+    kr = s3keyring.s3.S3Keyring(config_file=ini)
+    ctx.obj = {"keyring": kr}
 
 
 @main.command()
-@click.option('--ask/--no-ask', default=True)
+@click.option("--ask/--no-ask", default=True)
 @click.option("--local/--no-local",
               help="Save configuration in a file under the current directory",
               default=False)
@@ -40,32 +32,40 @@ def configure(ctx, ask, local):
 
 
 @main.command()
-@click.argument('service')
-@click.argument('username')
+@click.argument("service")
+@click.argument("key")
 @click.pass_context
-def get(ctx, service, username):
-    """Get password for a service/username."""
-    click.echo(ctx.obj['keyring'].get_password(service, username))
+def get(ctx, service, key):
+    """Get a secret."""
+    click.echo(ctx.obj["keyring"].get_password(service, key))
 
 
 @main.command()
-@click.argument('service')
-@click.argument('username')
-@click.argument('password')
+@click.argument("service")
+@click.argument("key")
+@click.argument("secret")
 @click.pass_context
-def set(ctx, service, username, password):
-    """Set a password for a service/username."""
-    click.echo(ctx.obj['keyring'].set_password(service, username, password))
+def set(ctx, service, key, secret):
+    """Set a secret."""
+    click.echo(ctx.obj["keyring"].set_password(service, key, secret))
+
+
+@main.command(name="list-keys")
+@click.argument("service")
+@click.pass_context
+def list_keys(ctx, service):
+    """List the keys associated to a given service."""
+    click.echo(ctx.obj["keyring"].list_keys(service))
 
 
 @main.command()
-@click.argument('service')
-@click.argument('username')
+@click.argument("service")
+@click.argument("key")
 @click.pass_context
-def delete(ctx, service, username):
-    """Deletes a password for a service/username."""
-    click.echo(ctx.obj['keyring'].delete_password(service, username))
+def delete(ctx, service, key):
+    """Delete a secret."""
+    click.echo(ctx.obj["keyring"].delete_password(service, key))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
