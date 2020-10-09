fb4254170da3472814e17e3af1a97d3aa26b2307
rb-nr <rb-nr@users.noreply.github.com>
2020-10-09 10:44:09 -0400

    Fix for python3

--- s3keyring/metadata.py
+++ s3keyring/metadata.py
@@ -8,11 +8,11 @@ Information describing the project.
 package = 's3keyring'
 project = "S3 backend for the keyring module"
 project_no_spaces = project.replace(' ', '')
-version = '0.7.1.post1'
+version = '0.7.2'
 description = 'Keeps your secrets safe in S3'
 authors = ['German Gomez-Herrero']
 authors_string = ', '.join(authors)
 emails = ['german@innovativetravel.eu']
 license = 'MIT'
 copyright = '2015 ' + authors_string
-url = 'http://github.com/InnovativeTravel/s3-keyring'
+url = 'http://github.com/Adroll/s3-keyring'
